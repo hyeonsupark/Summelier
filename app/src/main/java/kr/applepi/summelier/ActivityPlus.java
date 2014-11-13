b307acb083643f0fb9b39b7d5682c543803465c2
Heegyu Kim <heekue83@gmail.com>
2014-11-13 16:47:01 +0900

    SignIn SignUp completed

---
+++ app/src/main/java/kr/applepi/summelier/ActivityPlus.java
@@ -0,0 +1,79 @@
+package kr.applepi.summelier;
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.text.Editable;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+/**
+ * Created by KimHeekue on 2014-11-12.
+ */
+public abstract class ActivityPlus
+		extends Activity
+{
+
+	public View view_(int id)
+	{
+		return findViewById(id);
+	}
+
+	public void onClick(int id, View.OnClickListener lis)
+	{
+		view_(id).setOnClickListener(lis);
+	}
+
+	public void toast(String text, int duration)
+	{
+		Toast.makeText(this, text, duration).show();
+	}
+	public void alert(String title, String message, DialogInterface.OnClickListener lis) {
+		AlertDialog.Builder alert = new AlertDialog.Builder(this);
+
+
+		alert.setPositiveButton(android.R.string.ok, lis);
+		alert.setTitle(title);
+		alert.setMessage(message);
+		alert.show();
+	}
+
+
+
+
+	public TextView textView_(int id)
+	{
+		return (TextView)findViewById(id);
+	}
+
+	public String text_(int id)
+	{
+		return textView_(id).getText().toString();
+	}
+
+	public void text_(int id, Object text)
+	{
+		textView_(id).setText(text.toString());
+	}
+
+	public void text_f(int id, String format, Object ...args)
+	{
+		text_(id, String.format(format, args));
+	}
+
+
+	public EditText editText_(int id)
+	{
+		return (EditText)view_(id);
+	}
+	public Editable edit_(int id)
+	{
+		return editText_(id).getText();
+	}
+	public void edit_(int id, String text)
+	{
+		editText_(id).setText(text);
+	}
+}
--- app/src/main/java/kr/applepi/summelier/SignUpActivity.java
+++ app/src/main/java/kr/applepi/summelier/SignUpActivity.java
@@ -1,18 +1,91 @@
 package kr.applepi.summelier;
 
 import android.app.Activity;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.view.Window;
+import android.widget.Toast;
+
+import org.json.JSONObject;
+
+import kr.applepi.summelier.api.Api;
+import kr.applepi.summelier.api.ResultListener;
 
 // 사인업하면 액티비티 종료하고 로그인 화면보내삼
 
-public class SignUpActivity extends Activity {
+public class SignUpActivity extends ActivityPlus {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_sign_up);
+
+
+		onClick(R.id.sign_up, new View.OnClickListener() {
+			@Override
+			public void onClick(View view) {
+				trySignUp();
+			}
+		});
     }
+
+
+	private void trySignUp(){
+		String id = edit_(R.id.sign_up_et_id).toString(),
+				name = edit_(R.id.sign_up_et_name).toString(),
+				pw1 = edit_(R.id.sign_up_et_password1).toString(),
+				pw2 = edit_(R.id.sign_up_et_password2).toString();
+
+		if(!pw1.equals(pw2))
+		{
+			toast("두 비밀번호가 맞지 않아요.", Toast.LENGTH_LONG);
+			return;
+		}
+
+		if(!validateText(pw1)) {
+			toast("이상한 비밀번호에요", Toast.LENGTH_LONG);
+			return;
+		}
+
+		if(!validateText(id)) {
+			toast("이상한 아이디에요", Toast.LENGTH_LONG);
+			return;
+		}
+
+		Api api = Api.get(this);
+		api.registerBySummelier(id, pw1, name, new ResultListener()
+		{
+			@Override
+			public void onResult(boolean ok, JSONObject res) throws Exception
+			{
+				Log.d("로그인 결과", res.toString());
+
+				if (ok)
+				{
+					alert("",
+						"회원가입이 완료되었습니다. 로그인해 주세요.",
+						new DialogInterface.OnClickListener() {
+							@Override
+							public void onClick(DialogInterface dialogInterface, int i) {
+								finish();
+							}
+						});
+
+				} else {
+					toast("로그인이 안되네요. " + res.getString("message"), Toast.LENGTH_LONG);
+				}
+			}
+		});
+	}
+
+	private boolean validateText(String text)
+	{
+		return !text.contains(" ") && text.length() >= 4;
+	}
+
 
 }
