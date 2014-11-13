0fdd24dde3cd4332af5fd490965b19f4c44ca14a
Hyunsuuu <bingraa@gmail.com>
2014-11-13 17:35:15 +0900

    Add Profile

--- app/src/main/java/kr/applepi/summelier/auth/SplashActivity.java
+++ app/src/main/java/kr/applepi/summelier/auth/SplashActivity.java
@@ -24,6 +24,7 @@ public class SplashActivity extends ActivityPlus {
     EditText etId, etPassword;
     Button btnSignUp, btnSignIn;
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
